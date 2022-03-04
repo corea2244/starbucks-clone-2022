466894ecc51573dab82c8a00191736668d5a7de6
corea2244 <59365194+corea2244@users.noreply.github.com>
2022-03-04 23:36:34 +0900

    07. 헤덩와 드롭다운 메뉴 - 검색

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const searchEl = document.querySelector('.header__submenu__item__search');
+const searchInputEl = document.querySelector('.header__submenu__item__search__input');
+
+searchEl.addEventListener('click', function(){
+    searchInputEl.focus();
+});
+
+searchInputEl.addEventListener('focus', function(){
+    searchEl.classList.add('focused');
+    searchInputEl.setAttribute('placeholder', '통합검색');
+});
+
+searchInputEl.addEventListener('blur', function(){
+    searchEl.classList.remove('focused');
+    searchInputEl.setAttribute('placeholder', '');
+});
